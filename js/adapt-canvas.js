0099c6f0668a37c66ad990048866e1c984fb2559
Robert Peek <robert@delta-net.co.uk>
2016-01-31 21:50:03 +0000

    Initial canvas test

---
+++ js/adapt-canvas.js
@@ -0,0 +1,103 @@
+define(function(require) {
+
+    var ComponentView = require('coreViews/componentView');
+    var Adapt = require('coreJS/adapt');
+
+    var Canvas = ComponentView.extend({
+
+        preRender: function() {
+            this.listenTo(Adapt, 'device:changed', this.resizeImage);
+
+            // Checks to see if the graphic should be reset on revisit
+            this.checkIfResetOnRevisit();
+            // Listen for text change on audio extension
+            this.listenTo(Adapt, "audio:changeText", this.replaceText);
+        },
+
+        postRender: function() {
+            this.resizeImage(Adapt.device.screenSize);
+            this.$('.component-widget').on('inview', _.bind(this.inview, this));
+
+            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+                this.replaceText(Adapt.audio.textSize);
+            }
+
+            this.draw();
+        },
+
+        // Used to check if the graphic should reset on revisit
+        checkIfResetOnRevisit: function() {
+            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+
+            // If reset is enabled set defaults
+            if (isResetOnRevisit) {
+                this.model.reset(isResetOnRevisit);
+            }
+        },
+
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+
+                if (this._isVisibleTop && this._isVisibleBottom) {
+                    this.$('.component-widget').off('inview');
+                    this.setCompletionStatus();
+                }
+
+            }
+        },
+
+        resizeImage: function(width) {
+            var imageWidth = width === 'medium' ? 'small' : width;
+            this.$('.graphic-widget img').attr('src', this.model.get('_graphic')[imageWidth]);
+
+            this.$('.graphic-widget').imageready(_.bind(function() {
+                this.setReadyStatus();
+            }, this));
+        },
+
+        // Reduced text
+        replaceText: function(value) {
+            // If enabled
+            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+                if(value == 0) {
+                    this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
+                    this.$('.component-body-inner').html(this.model.get('body')).a11y_text();
+                } else {
+                    this.$('.component-title-inner').html(this.model.get('displayTitleReduced')).a11y_text();
+                    this.$('.component-body-inner').html(this.model.get('bodyReduced')).a11y_text();
+                }
+            }
+        },
+
+        draw: function() {
+          var canvas = document.getElementById("canvas");
+          if (canvas.getContext) {
+            var ctx = canvas.getContext("2d");
+
+            ctx.fillStyle = "rgb(200,0,0)";
+            ctx.fillRect (10, 10, 55, 50);
+
+            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+            ctx.fillRect (30, 30, 55, 50);
+          }
+        }
+
+
+
+
+
+    });
+
+    Adapt.register('canvas', Canvas);
+
+    return Canvas;
+
+});
